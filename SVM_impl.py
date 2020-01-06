b5e23c4f3c2b917465fd0313b8a35c635204ddf9
Chaos-Sheeza <jesmar16@live.com>
2020-01-06 17:10:56 +0100

    First commit

---
+++ SVM_impl.py
@@ -0,0 +1,21 @@
+from sklearn import svm, datasets
+
+numbers = datasets.load_digits()
+
+#-----------------------------------------------------------------------
+#                          Testing functions
+#-----------------------------------------------------------------------
+
+'''
+#data
+print(numbers.images)
+#classes
+print(numbers.target)
+#testing zip
+print(list(zip(numbers.images, numbers.target)))
+'''
+
+#-----------------------------------------------------------------------
+#                               Main code
+#-----------------------------------------------------------------------
+
